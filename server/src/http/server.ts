9a3ae6d3360a64c624082fd7d5e031ed887539f6
illyanaAvelar <illyavelar012@gmail.com>
2024-09-11 15:47:51 -0300

    get week summary route

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -8,6 +8,7 @@ import {
 import { createGoalRoute } from './routes/create-goal'
 import { createGoalCompletionRoute } from './routes/create-goal-completion'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,6 +20,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createGoalRoute)
 app.register(createGoalCompletionRoute)
 app.register(getWeekSummaryRoute)
+app.register(getWeekPendingGoalsRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
