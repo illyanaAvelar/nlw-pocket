8615d6d18b2aee127d572e9cefd36f7e33b53a99
illyanaAvelar <illyavelar012@gmail.com>
2024-09-11 11:06:14 -0300

    create goal completion function

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,11 +7,33 @@ import {
 } from 'fastify-type-provider-zod'
 import z from "zod";
 import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    const { goalCompletion } = await createGoalCompletion({
+      goalId,
+    })
+
+    return { goalCompletionId: goalCompletion.id }
+  }
+)
+
 
 app.get('/pending-goals', async () => {
   const { pendingGoals } = await getWeekPendingGoals()
